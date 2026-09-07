13d6ca69cd4dec5e286bf0ec449d267a33eeaa11
maxwellwire <146187391+maxwellwire@users.noreply.github.com>
2026-09-07 21:14:45 +0100

    Create test-signup.sh

---
+++ test-signup.sh
@@ -0,0 +1,1 @@
+curl -X POST http://localhost:3000/api/auth/signup -H "Content-Type: application/json" -d "{\"firstName\":\"Test\",\"lastName\":\"User\",\"username\":\"testuser1\",\"email\":\"benjamincyril2005@gmail.com\",\"password\":\"testpassword123\"}"
