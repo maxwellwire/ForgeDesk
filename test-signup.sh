b9d904e95ea4e372b8f51c684ba01e2a570bc50b
maxwellwire <146187391+maxwellwire@users.noreply.github.com>
2026-09-09 06:03:34 -0400

    Update test-signup.sh

--- test-signup.sh
+++ test-signup.sh
@@ -1,3 +1,3 @@
 curl -X POST http://localhost:3000/api/auth/signup \
   -H "Content-Type: application/json" \
-  -d '{"firstName":"Test","lastName":"User","username":"testuser2","email":"YOUR_web3terdue@gmail.com","password":"testpassword123"}'
+  -d '{"firstName":"Test","lastName":"User","username":"testuser3","email":"important20055@gmail.com","password":"testpassword123"}'
