5c300ed633ae284c5fbfc2e332ac420b9b041f2d
maxwellwire <146187391+maxwellwire@users.noreply.github.com>
2026-09-10 00:48:20 +0100

    Update test-participate-verified.sh

--- test-participate-verified.sh
+++ test-participate-verified.sh
@@ -1,2 +1,2 @@
-curl -X POST http://localhost:3000/api/participations/cmtuow9oq00279j0i6ra9ntb \
+curl -X POST http://localhost:3000/api/participations/cmtuow9oq000279j0i6ra9ntb \
   -b login-cookies.txt
