3aad82ae1c2a96b463c89e67274665605d178d41
maxwellwire <146187391+maxwellwire@users.noreply.github.com>
2026-09-10 00:55:09 +0100

    Update test-submit.sh

--- test-submit.sh
+++ test-submit.sh
@@ -1,4 +1,4 @@
 curl -X POST http://localhost:3000/api/submissions \
   -H "Content-Type: application/json" \
   -b login-cookies.txt \
-  -d '{"campaignId":"cmtuow9oq000279j0i6ra9ntb","taskId":"PASTE_YOUR_ACTUAL_TASK_ID_HERE","proofUrl":"https://twitter.com/example/status/123"}'
+  -d '{"campaignId":"cmtuow9oq000279j0i6ra9ntb","taskId":"cmtuqmeei00017kagy2qslfh5","proofUrl":"https://twitter.com/example/status/123"}'
